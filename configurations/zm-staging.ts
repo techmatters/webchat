7ac7c61eb6ecf98ce9bd3f9e8024fe8e807435f8
Andrés Virga <andvirga@gmail.com>
2021-04-09 18:16:46 -0300

    Added Zambia translated messages

--- configurations/zm-staging.ts
+++ configurations/zm-staging.ts
@@ -2,15 +2,82 @@ import { PreEngagementConfig, Translations, Configuration, MapHelplineLanguage }
 
 const accountSid = 'ACc59300c7ca018e8652e4d6d86c2d50e6';
 const flexFlowSid = 'FObb9dfe97f1c59f455ab01811bec74cd5';
-const defaultLanguage = 'en-US';
+const defaultLanguage = 'Bemba';
 const captureIp = true;
 
 const translations: Translations = {
   'en-US': {
+    MessageInputDisabledReasonHold: "We'll transfer you now. Please hold for a counsellor.",
+    EntryPointTagLine: "Chat with us",
+    PreEngagementDescription: "Let's get started",
+    Today: "Today",
+    InputPlaceHolder: "Type Message",
     WelcomeMessage: "Welcome to ChildLine Zambia!",
+    Yesterday: "Yesterday",
+    TypingIndicator: "Counselor is typing",
+    MessageCanvasTrayButton: "Start New Chat",
     MessageCanvasTrayContent: "<p>The counselor has left the chat. Thank you for reaching out. Please contact us again if you need more help.</p>",
-    MessageInputDisabledReasonHold: "Please hold for a counselor.",
     AutoFirstMessage: "Incoming webchat contact",
+  },
+  'Bemba': {
+    MessageInputDisabledReasonHold: "Twalamutuma nomba kuli Chimbusa, pembeleni ichimpusa nomba.",
+    EntryPointTagLine: "Landeni naifwe",
+    PreEngagementDescription: "tiyeni twambeko ilyashi",
+    Today: "Lelo",
+    InputPlaceHolder: "Taipeni ilyashi",
+    WelcomeMessage: "Mwaiseni kuli ChildLine Zambia!",
+    Yesterday: "Mailo",
+    TypingIndicator: "Ichimbusa chile taipa ilyashi",
+    MessageCanvasTrayButton: "Yambeni kutaipa ilyashi imbi",
+    MessageCanvasTrayContent: "Ichimbusa chapwisha imilimo yakulanda.Twatotela pakutuma kuno.Mukatutumine nakabili nga mwafwaya ubwafwilisho kuli baifwe.",
+  },
+  'Tonga': {
+    MessageInputDisabledReasonHold: "Tulamuswaanganya lino asikuyumya-yumya/sikulaya. Amujatilile notucimuswaanganya.",
+    EntryPointTagLine: "Amubandike andiswe",
+    PreEngagementDescription: "Atukanke/atutalike",
+    Today: "Sunu",
+    InputPlaceHolder: "Lemba kang'wadi/kagwalo",
+    WelcomeMessage: "Mwatambulwa ku ChildLine Zambia!",
+    Yesterday: "Jilo",
+    TypingIndicator: "Sikuyumyayumya watalika kulemba",
+    MessageCanvasTrayButton: "Talika mubandi mupya",
+    MessageCanvasTrayContent: "Sikulaya/sikuyumyayumyawazwa lino amubandi. Twalumba kukwabana andiswe,Inga mwatuma lubo naa muciyanda lumbi lugwasyo.",
+  },
+  'Lunda': {
+    MessageInputDisabledReasonHold: "Chuna kuitemesha ahembeleliku chanti kundi ankhong'u.",
+    EntryPointTagLine: "Tuhanjiki mwani",
+    PreEngagementDescription: "Tutachikiku",
+    Today: "Lelu",
+    InputPlaceHolder: "Sonekenu Muzhimbu",
+    WelcomeMessage: "Shikenu mwani kuchota cha ChildLine Zambia!",
+    Yesterday: "Haloshi",
+    TypingIndicator: "Nkhong'u nakusoneka Muzhimbu",
+    MessageCanvasTrayButton: "Tachikenu kuhanjika",
+    MessageCanvasTrayContent: "Ankhong'u adihu wanyi kusakililaku mwani, hakushika kudechu, mwani anachweshi kuchuma mpinji.",
+  },
+  'Nyanja': {
+    MessageInputDisabledReasonHold: "Chonde tipeleka lamya lanu ku wa uphungu telo dikilani.",
+    EntryPointTagLine: "Lankhulisanani nafe!",
+    PreEngagementDescription: "Tiyeni tiyembe kulankhulisana",
+    Today: "Lelo",
+    InputPlaceHolder: "Lembani zimene zimene mufuna kulemba",
+    WelcomeMessage: "Mwalandilidwa kuno ku ChildLine Zambia!",
+    Yesterday: "Dzulo!",
+    TypingIndicator: "Wauphungu alikulemba!",
+    MessageCanvasTrayButton: "Yambani nkhani ina!",
+    MessageCanvasTrayContent: "Wauphungu achoka mkukambitsana nanu. Zikomo kwambili pa kukwanisa.",
+  },
+  'Kaonde': {
+    MessageInputDisabledReasonHold: "Tusakwimi tuma pembelelai, pacheche ba nkwasho.",
+    EntryPointTagLine: "Isambai natweba",
+    PreEngagementDescription: "Twayayi tutatule",
+    Today: "Lelo",
+    InputPlaceHolder: "Lembayi mulubwe",
+    WelcomeMessage: "Mwaiyayi mwani ku ChildLine Zambia!",
+    Yesterday: "Kesha",
+    TypingIndicator: "Nkwasho wena kunemba",
+    MessageCanvasTrayButton: "Tatulayi kwisamba kipya kipya",
+    MessageCanvasTrayContent: "Nkwasho wafumapo twasanta pakufika kwiatweba, mwakonsha kutuma lamya.",
   },
 };
 
