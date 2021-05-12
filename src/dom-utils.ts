aa71c665c46c85dcec8dd19c56f18a3713bffc1d
Murilo Machado <murilovmachado@gmail.com>
2021-05-12 19:00:12 +0100

    Read z-index parameter before creating Twilio's WebChat

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,12 +1,13 @@
 const CONTAINER_ID = 'twilio-customer-frame';
 
+export type ScriptParameter = string | null | undefined;
+
 /**
  * Updates Webchat container z-index with the value provided by the client.
  * Sample: <script src="point/to/aselo-webchat.min.js" data-z-index="200">
  */
-export function updateZIndex() {
+export function updateZIndex(zIndex: ScriptParameter) {
   const container = document?.getElementById(CONTAINER_ID);
-  const zIndex = document?.currentScript?.getAttribute('data-z-index');
 
   if (container && zIndex) {
     container.style.zIndex = zIndex;
