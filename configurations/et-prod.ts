4502b470b5bbb995620c304b82fe9f6d17adbe5c
murilovmachado <murilovmachado@gmail.com>
2021-09-15 15:42:45 +0100

    Setup ET and MW Prod (#51)

---
+++ configurations/et-prod.ts
@@ -0,0 +1,11 @@
+import { Configuration } from './types';
+import { config as etStaging } from './et-staging';
+
+const accountSid = 'AC16aa1fb9123860efd3bdde74de556e21';
+const flexFlowSid = 'FOb18e8dca4c6322d2abe701d159ea77eb';
+
+export const config: Configuration = {
+  ...etStaging,
+  accountSid,
+  flexFlowSid,
+};
