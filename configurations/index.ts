c25efcfca197f0a76b56a92bb4544f7f27573154
Murilo Machado <murilovmachado@gmail.com>
2021-12-13 11:45:36 +0000

    Mising config for br-prod

--- configurations/index.ts
+++ configurations/index.ts
@@ -11,11 +11,12 @@ import { config as etStaging } from './et-staging';
 import { config as mwStaging } from './mw-staging';
 import { config as etProd } from './et-prod';
 import { config as mwProd } from './mw-prod';
+import { config as brProd } from './br-prod';
 
 const rawConfig = webpack.env.CONFIG;
 checkCONFIG(rawConfig);
 
-type PresetConfig = 'dev' | 'test-staging' | 'beta' | 'zm-staging' | 'zm-prod' | 'za-staging' | 'za-prod' | 'et-staging' | 'mw-staging' | 'et-prod' | 'mw-prod';
+type PresetConfig = 'dev' | 'test-staging' | 'beta' | 'zm-staging' | 'zm-prod' | 'za-staging' | 'za-prod' | 'et-staging' | 'mw-staging' | 'et-prod' | 'mw-prod' | 'br-prod';
 const config = rawConfig as PresetConfig;
 
 type ConfigMap = {
@@ -34,6 +35,7 @@ const configMap: ConfigMap = {
   'mw-staging': mwStaging,
   'et-prod': etProd,
   'mw-prod': mwProd,
+  'br-prod': brProd,
 };
 
 export const getCurrentConfig = (): Configuration => configMap[config];
