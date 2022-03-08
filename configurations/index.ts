ae148cf2d4ab2787c698d827375c7338184626b5
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2022-03-08 11:02:31 +0200

    Update index.ts
    Add ph-staging config

--- configurations/index.ts
+++ configurations/index.ts
@@ -14,11 +14,12 @@ import { config as mwProd } from './mw-prod';
 import { config as jmStaging } from './jm-staging';
 import { config as jmProd } from './jm-prod';
 import { config as caStaging } from './ca-staging';
+import { config as phStaging } from './ph-staging';
 
 const rawConfig = webpack.env.CONFIG;
 checkCONFIG(rawConfig);
 
-type PresetConfig = 'dev' | 'test-staging' | 'beta' | 'zm-staging' | 'zm-prod' | 'za-staging' | 'za-prod' | 'et-staging' | 'mw-staging' | 'et-prod' | 'mw-prod' | 'jm-staging' | 'jm-prod' | 'ca-staging';
+type PresetConfig = 'dev' | 'test-staging' | 'beta' | 'zm-staging' | 'zm-prod' | 'za-staging' | 'za-prod' | 'et-staging' | 'mw-staging' | 'et-prod' | 'mw-prod' | 'jm-staging' | 'jm-prod' | 'ca-staging' | 'ph-staging';
 const config = rawConfig as PresetConfig;
 
 type ConfigMap = {
@@ -39,7 +40,8 @@ const configMap: ConfigMap = {
   'mw-prod': mwProd,
   'jm-staging': jmStaging,
   'jm-prod': jmProd,
-  'ca-staging': caStaging
+  'ca-staging': caStaging,
+  'ph-staging': phStaging
 };
 
 export const getCurrentConfig = (): Configuration => configMap[config];
