2cdc6ab1b1b59a8c508800de9dbd4288169407fb
Stephen Hand <steveh@techmatters.org>
2022-05-26 16:44:07 +0100

    Added chat deployment for e2e tests

---
+++ configurations/e2e-development.ts
@@ -0,0 +1,69 @@
+import {
+  PreEngagementConfig,
+  Translations,
+  Configuration,
+  MapHelplineLanguage,
+} from './types';
+
+const accountSid = 'AC6a65d4fbbc731e64e1c94e9806675c3b';
+const flexFlowSid = 'FOfce25fd1dff726dcdae2899de86de6c5';
+const defaultLanguage = 'en-US';
+const captureIp = true;
+
+const translations: Translations = {
+  'en-US': {
+    MessageInputDisabledReasonHold:
+      "We'll transfer you now. Please hold for a counsellor.",
+    EntryPointTagLine: 'Chat with us',
+    PreEngagementDescription: "Let's get started",
+    Today: 'Today',
+    InputPlaceHolder: 'Type Message',
+    WelcomeMessage: 'Welcome to ChildLine Zambia!',
+    Yesterday: 'Yesterday',
+    TypingIndicator: 'Counselor is typing',
+    MessageCanvasTrayButton: 'Start New Chat',
+    MessageCanvasTrayContent: '',
+    AutoFirstMessage: 'Incoming webchat contact from',
+    StartChat: 'Start Chat!',
+  },
+};
+
+const preEngagementConfig: PreEngagementConfig = {
+  description: "Let's get started",
+  fields: [
+    {
+      label: 'Hidden Field',
+      type: 'InputField',
+      attributes: {
+        name: '',
+        readOnly: true,
+      },
+    },
+  ],
+  submitLabel: 'Start Chat!',
+};
+
+const memberDisplayOptions = {
+  yourDefaultName: 'You',
+  yourFriendlyNameOverride: false,
+  theirFriendlyNameOverride: false,
+  theirDefaultName: 'End To End Test Counsellor',
+}
+
+const mapHelplineLanguage: MapHelplineLanguage = helpline => {
+  switch (helpline) {
+    default:
+      return defaultLanguage;
+  }
+}
+
+export const config: Configuration = {
+  accountSid,
+  flexFlowSid,
+  defaultLanguage,
+  translations,
+  preEngagementConfig,
+  mapHelplineLanguage,
+  memberDisplayOptions,
+  captureIp,
+};
