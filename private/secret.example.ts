f1eb9b652b7f7022b165ba6de9720b30ded7e7f2
mythilytm <mythily@techmatters.org>
2022-07-12 14:21:43 -0400

    Added instructions for secret example to use serverless url

--- private/secret.example.ts
+++ private/secret.example.ts
@@ -1,4 +1,5 @@
 /**
- * Create a local file named 'secret.ts' that exports an API_KEY
+ * Create a local file named 'secret.ts' that exports an API_KEY and SERVERLESS_URL
  */
 export const API_KEY = 'xxxxxxxxxxxxxxxxx'; // IP Find API Key
+export const SERVERLESS_URL = 'https://serverlexxxxxxxx-xxx.twil.io'
