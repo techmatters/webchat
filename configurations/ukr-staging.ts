9b158b22565ad1ceceab8f328240f3440677edc8
Dee Luo <79488570+dee-luo@users.noreply.github.com>
2022-09-01 16:33:03 -0400

    Update ukr-staging.ts

--- configurations/ukr-staging.ts
+++ configurations/ukr-staging.ts
@@ -2,32 +2,104 @@ import { PreEngagementConfig, Translations, Configuration, MapHelplineLanguage }
 
 const accountSid = 'ACbdbee34ef7d099e71cf095d540ff3270';
 const flexFlowSid = 'FO9d20dbe99abbc3b9ad7709f961b0fe95';
-const defaultLanguage = 'en-US';
-const captureIp = true;
+const defaultLanguage = 'hu-HU';
+const captureIp = false;
 
 const translations: Translations = {
-  'en-US': {
-    WelcomeMessage: 'Welcome to the International Ukraine Support Helpline',
-    MessageCanvasTrayContent: '',
-    MessageInputDisabledReasonHold: 'Please hold for a counselor.',
-    AutoFirstMessage: 'Incoming webchat contact from',
-  },
-};
+    'en-US': {
+      MessageInputDisabledReasonHold:
+        "We'll transfer you now. Please hold for a counsellor.",
+      EntryPointTagLine: 'Chat with us',
+      PreEngagementDescription: "Let's get started",
+      Today: 'Today',
+      InputPlaceHolder: 'Type Message',
+      WelcomeMessage: 'Welcome to ChildLine Zambia!',
+      Yesterday: 'Yesterday',
+      TypingIndicator: 'Counselor is typing',
+      MessageCanvasTrayButton: 'Start New Chat',
+      MessageCanvasTrayContent: '',
+      AutoFirstMessage: 'Incoming webchat contact from',
+      StartChat: 'Start Chat!',
+    },
+    'hu-HU': {
+      MessageInputDisabledReasonHold:
+        'Továbbítunk egy ügyelőhöz, akivel beszélgetni tudsz.',
+      EntryPointTagLine: 'Csetelj velünk',
+      PreEngagementDescription: 'Kezdjük el',
+      Today: 'Ma',
+      InputPlaceHolder: 'Taipeni ilyashi',
+      WelcomeMessage: 'Szia, ez itt a Kék Vonal!',
+      Yesterday: 'Tegnap',
+      TypingIndicator: 'gépelés...',
+      MessageCanvasTrayButton: 'Chat indítása',
+      MessageCanvasTrayContent: '',
+      AutoFirstMessage: 'Bejövő chat',
+      StartChat: 'Chat indítása!',
+    },
+    'ukr-HU': {
+      MessageInputDisabledReasonHold:
+        'Зв\'яжемо тебе із нашим консультантом, з яким ти зможеш поговорити.',
+      EntryPointTagLine: 'Поспілкуйся з нами в чаті',
+      PreEngagementDescription: 'Давайте розпочнемо',
+      Today: 'Сьогодні',
+      InputPlaceHolder: "Введіть повідомлення",
+      WelcomeMessage: 'Привіт, це Блакитна Лінія!',
+      Yesterday: 'вчора',
+      TypingIndicator: 'набір тексту...',
+      MessageCanvasTrayButton: 'Почати чат',
+      MessageCanvasTrayContent: '',
+      AutoFirstMessage: 'Вхідний чат',
+      StartChat: 'Почати чат!',
+    },
+    'ru-HU': {
+      MessageInputDisabledReasonHold:
+        "Свяжем тебя с нашим консультантом, с которым ты сможешь поговорить.",
+      EntryPointTagLine: 'Пообщайся с нами в чате',
+      PreEngagementDescription: 'Давайте начнем',
+      Today: 'Сегодня',
+      InputPlaceHolder: 'Введите сообщение',
+      WelcomeMessage: 'Привет, это Синяя Линия!',
+      Yesterday: 'Вчерашний день',
+      TypingIndicator: "набор текста...",
+      MessageCanvasTrayButton: 'Привет, это Синяя Линия!',
+      MessageCanvasTrayContent: '',
+      AutoFirstMessage: 'Входящий чат',
+      StartChat: 'Начать чат!',
+    }
+  };
 
 const preEngagementConfig: PreEngagementConfig = {
-  description: "Let's get started",
-  fields: [
-    {
-      label: 'Hidden Field',
-      type: 'InputField',
-      attributes: {
-        name: '',
-        readOnly: true,
+    description: "Kezdjük el",
+    fields: [
+      {
+        label: 'Válaszd ki a nyelved',
+        type: 'SelectItem',
+        attributes: {
+          name: 'language',
+          required: true,
+          readOnly: false,
+        },
+        options: [
+          {
+            value: 'hu-HU',
+            label: '1. Hungarian',
+            selected: true,
+          },
+          {
+            value: 'ukr-HU',
+            label: '2. Ukrain',
+            selected: false,
+          },
+          {
+            value: 'ru-HU',
+            label: '3. Russain',
+            selected: false,
+          }
+        ],
       },
-    },
-  ],
-  submitLabel: 'Start Chat!',
-};
+    ],
+    submitLabel: 'Chat indítása',
+  };
 
 const mapHelplineLanguage: MapHelplineLanguage = helpline => {
   switch (helpline) {
@@ -40,7 +112,7 @@ const memberDisplayOptions = {
   yourDefaultName: 'You',
   yourFriendlyNameOverride: false,
   theirFriendlyNameOverride: false,
-  theirDefaultName: 'Counsellor',
+  theirDefaultName: 'Kék Vonal',
 }
 
 export const config: Configuration = {
