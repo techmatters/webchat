babe5db1f3ae4966cf56c599a133512aec18463c
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2022-12-19 18:14:36 +0200

    Update mt-staging.ts

--- configurations/mt-staging.ts
+++ configurations/mt-staging.ts
@@ -13,18 +13,48 @@ const captureIp = true;
 const translations: Translations = {
   'en-US': {
     MessageInputDisabledReasonHold:
-      "We'll transfer you now. Please hold for a counsellor.",
+      "We'll transfer you now. Please hold for a support mentor.",
     EntryPointTagLine: 'Chat with us',
     PreEngagementDescription: "Let's get started",
     Today: 'Today',
     InputPlaceHolder: 'Type Message',
     WelcomeMessage: 'Welcome to Kellimni!',
     Yesterday: 'Yesterday',
-    TypingIndicator: 'Counselor is typing',
+    TypingIndicator: 'Support mentor is typing',
     MessageCanvasTrayButton: 'Start New Chat',
     MessageCanvasTrayContent: '',
     AutoFirstMessage: 'Incoming webchat contact from',
     StartChat: 'Start Chat!',
+  },
+  'mt-MT': {
+    MessageInputDisabledReasonHold:
+      "Ha nittrasferuk lil wieħed mis-Support Mentors tagħna.",
+    EntryPointTagLine: 'Chat magħna',
+    PreEngagementDescription: "Ejja nibdew",
+    Today: 'Illum',
+    InputPlaceHolder: 'Tip Messaġġ',
+    WelcomeMessage: 'Merħba lil Kellimni!',
+    Yesterday: 'Ilbieraħ',
+    TypingIndicator: 'Il support mentor qed jittajpja',
+    MessageCanvasTrayButton: 'Ibda Chat Ġdida',
+    MessageCanvasTrayContent: '',
+    AutoFirstMessage: 'Incoming webchat contact from',
+    StartChat: 'Ibda Chat!',
+  },
+  'ukr-MT': {
+      MessageInputDisabledReasonHold:
+        'Зв\'яжемо тебе із нашим консультантом, з яким ти зможеш поговорити.',
+      EntryPointTagLine: 'Поспілкуйся з нами в чаті',
+      PreEngagementDescription: 'Давайте розпочнемо',
+      Today: 'Сьогодні',
+      InputPlaceHolder: "Введіть повідомлення",
+      WelcomeMessage: 'Привіт, це Блакитна Лінія!',
+      Yesterday: 'вчора',
+      TypingIndicator: 'набір тексту...',
+      MessageCanvasTrayButton: 'Почати чат',
+      MessageCanvasTrayContent: '',
+      AutoFirstMessage: 'Вхідний чат',
+      StartChat: 'Почати чат!',
   },
 };
 
@@ -32,12 +62,30 @@ const preEngagementConfig: PreEngagementConfig = {
   description: "Let's get started",
   fields: [
     {
-      label: 'Hidden Field',
-      type: 'InputField',
+      label: 'Select your language',
+      type: 'SelectItem',
       attributes: {
-        name: '',
-        readOnly: true,
+        name: 'language',
+        required: true,
+        readOnly: false,
       },
+      options: [
+        {
+          value: 'English',
+          label: '1. English',
+          selected: true,
+        },
+        {
+          value: 'mt-MT',
+          label: '2. Maltese',
+          selected: false,
+        },
+        {
+          value: 'ukr-MT',
+          label: '3. Ukrainian',
+          selected: false,
+        },
+      ],
     },
   ],
   submitLabel: 'Start Chat!',
@@ -47,7 +95,7 @@ const memberDisplayOptions = {
   yourDefaultName: 'You',
   yourFriendlyNameOverride: false,
   theirFriendlyNameOverride: false,
-  theirDefaultName: 'Kellimni Counsellor',
+  theirDefaultName: 'Support Mentor',
 }
 
 const mapHelplineLanguage: MapHelplineLanguage = helpline => {
