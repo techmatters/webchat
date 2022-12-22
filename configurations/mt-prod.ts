29a283c9445b3a5cf20bbcbe2ad96b7715acaf1d
Humairaa127 <41548706+Humairaa127@users.noreply.github.com>
2022-12-22 18:19:33 +0200

    Update mt-prod.ts

--- configurations/mt-prod.ts
+++ configurations/mt-prod.ts
@@ -2,7 +2,7 @@ import { Configuration } from './types';
 import { config as mtStaging } from './mt-staging';
 
 const accountSid = 'AC7854f6126459347434a8e659295ebb79';
-const flexFlowSid = 'FO10f415b11f07012928cf198fac34f657';
+const flexFlowSid = 'FO46d948dc907e2e5d36b07a63bd1f0052';
 
 export const config: Configuration = {
   ...mtStaging,
