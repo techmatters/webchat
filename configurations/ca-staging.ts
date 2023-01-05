e7768e296faf7ac34fa562628320951ea20b6e3e
Dee Luo <79488570+dee-luo@users.noreply.github.com>
2023-01-05 16:02:20 -0500

    Update ca-staging.ts

--- configurations/ca-staging.ts
+++ configurations/ca-staging.ts
@@ -121,7 +121,7 @@ const contactType: ContactType = 'ip';
           },
           options: [
             {
-              value: 'asexual',
+              value: 'Asexual',
               label: 'Asexual',
               selected: true,
             },
