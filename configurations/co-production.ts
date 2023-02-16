63bad02b133ea0ec1d3706eb011bbd9de796d32c
mythilytm <mythily@techmatters.org>
2023-02-16 17:15:12 -0500

    Build fix - Change imports for prod configurations

--- configurations/co-production.ts
+++ configurations/co-production.ts
@@ -15,7 +15,7 @@
  */
 
 import { Configuration } from '../types';
-import { config as coStaging } from './co-staging';
+import { config as coStaging } from '../configurations/co-staging';
 
 const accountSid = 'AC520ec62dcfa4ab4105c2f5850caf52b0';
 const flexFlowSid = 'FOf1ace264485595c61b9d926657c105c0';
--- configurations/et-production.ts
+++ configurations/et-production.ts
@@ -15,7 +15,7 @@
  */
 
 import { Configuration } from '../types';
-import { config as etStaging } from './et-staging';
+import { config as etStaging } from '../configurations/et-staging';
 
 const accountSid = 'AC16aa1fb9123860efd3bdde74de556e21';
 const flexFlowSid = 'FOb18e8dca4c6322d2abe701d159ea77eb';
