3666bc4f2a73d6c52c0be1c9549a373ea154852c
mythilytm <mythily@techmatters.org>
2023-05-01 13:34:33 -0400

    workflow and config changes

--- configurations/as-development.ts
+++ configurations/as-development.ts
@@ -24,6 +24,7 @@ const captureIp = true;
 const checkOpenHours = true;
 const contactType = 'ip';
 const showEmojiPicker = true;
+const enableRecaptcha = true;
 
 const translations: Translations = {
   'en-US': {
@@ -225,5 +226,6 @@ export const config: Configuration = {
   contactType,
   showEmojiPicker,
   blockedEmojis,
-  memberDisplayOptions
+  memberDisplayOptions,
+  enableRecaptcha,
 };
