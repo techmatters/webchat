45fc252268a9e67407d794073e6b85d1b82ffa52
Robert Bo Davis <bo@interrobang.consulting>
2023-08-11 09:36:27 -0400

    fix: disable office hours for e2e, dev hot reload

--- src/pre-engagement-form/ReCaptcha/index.tsx
+++ src/pre-engagement-form/ReCaptcha/index.tsx
@@ -21,29 +21,24 @@ import { RECAPTCHA_KEY } from '../../../private/secret';
 import { validateUser } from './recaptchaValidation';
 
 type Props = {
-  bypassCaptcha: boolean | undefined;
   onRecaptchaChange: (verified: boolean) => void;
 };
 
-const ReCaptcha: React.FC<Props> = ({ bypassCaptcha, onRecaptchaChange }) => {
+const ReCaptcha: React.FC<Props> = ({ onRecaptchaChange }) => {
   const recaptchaRef = useRef<ReCAPTCHA>(null);
 
-  const onChange = (token: string | null) => {
+  const onChange = async (token: string | null) => {
     const verified = token !== null;
     onRecaptchaChange(verified);
 
     if (verified) {
       try {
-        validateUser(token ?? '');
+        await validateUser(token ?? '');
       } catch (error) {
         console.log(error);
       }
     }
   };
-
-  if (bypassCaptcha) {
-    return null;
-  }
 
   return <ReCAPTCHA sitekey={RECAPTCHA_KEY} size="normal" ref={recaptchaRef} onChange={onChange} />;
 };
